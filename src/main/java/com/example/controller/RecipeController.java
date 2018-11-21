e83547ce46f686dbd80a588b58b5bb7e9f6d8bc1
Charlie Burris <cburris1983@gmail.com>
2018-11-20 19:19:05 -0500

    Add controller with greeting endpoint stub.

---
+++ src/main/java/com/example/controller/RecipeController.java
@@ -0,0 +1,26 @@
+/**
+ *
+ */
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author cburr
+ *
+ */
+@Controller
+public class RecipeController {
+
+	// TODO here we will create the endpoints to post recipes too.
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
+}
